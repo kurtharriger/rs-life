16e0c10fc996cfcffbd73a1545c25deeb0e608a0
Kurt Harriger <kurtharriger@gmail.com>
2014-03-29 08:36:24 -0600

    Working version of get-neighbouring-indicies

--- src/clj_rs/core.clj
+++ src/clj_rs/core.clj
@@ -1,11 +1,18 @@
 (ns clj-rs.core)
 
+(defn valid-index? [row-size col-size row col]
+  (and (>= row 0) (>= col 0) (< row row-size) (< col col-size)))
+
 (defn get-neighbouring-indecies
-  [row-size col-size [row col]]
-  (for [[row-delta col-delta] [[-1 -1] [-1 0] [-1 1]
-                               [ 0 -1]        [ 0 1]
-                               [ 1 -1] [ 1 0] [ 1 1]]]
-    [(+ row row-delta) (+ col col-delta)]))
+  [[row-size col-size] [row col]]
+  {:pre [(valid-index? row-size col-size row col)]}
+  (filter
+   (fn [[new-row new-col]]
+     (valid-index? row-size col-size new-row new-col))
+   (for [[row-delta col-delta] [[-1 -1] [-1 0] [-1 1]
+                                [ 0 -1]        [ 0 1]
+                                [ 1 -1] [ 1 0] [ 1 1]]]
+     [(+ row row-delta) (+ col col-delta)])))
 
 
 
--- test/clj_rs/test/core.clj
+++ test/clj_rs/test/core.clj
@@ -17,5 +17,15 @@
              [1 0 0 0 1]])))))
 
 (deftest test-get-neighbouring-indecies
-  (is (= [[0 0] [0 1] [0 2] [1 0] [1 2] [2 0] [2 1] [2 2]]
-       (get-neighbouring-indecies 3 3 [1 1]))))
+  (testing "returns the 8 neigbours of a middle square"
+    (is (= [[0 0] [0 1] [0 2] [1 0] [1 2] [2 0] [2 1] [2 2]]
+           (get-neighbouring-indecies [3 3] [1 1]))))
+  (testing "does not return negative indecies for left and top edge"
+    (is (= [[0 1] [1 0] [1 1]]
+           (get-neighbouring-indecies [3 3] [0 0]))))
+  (testing "does not return indicies beyond right and bottom edge"
+    (is (= [[1 1] [1 2] [2 1]]
+           (get-neighbouring-indecies [3 3] [2 2]))))
+  (testing "throws exception if initial position is not on board"
+    (is (thrown? AssertionError
+                 (get-neighbouring-indecies [3 3] [3 3])))))
