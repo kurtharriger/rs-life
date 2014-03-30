a0dd13f19045077d888df351205dbc75f1a90772
Kurt Harriger <kurtharriger@gmail.com>
2014-03-29 18:42:44 -0600

    Auto advance game generation

--- src/cljs/rs_life/app.cljs
+++ src/cljs/rs_life/app.cljs
@@ -1,15 +1,9 @@
 (ns rs-life.app
-  (:require [rs-life.core :refer [next-generation]]))
+  (:require [cljs.core.async :as async :refer [<! >! chan put!]]
+            [rs-life.core :refer [next-generation]])
+  (:require-macros [cljs.core.async.macros :as m :refer [go go-loop alt!]]))
 
-(.text (js/$ "h1") "It's alive!!")
-
-
-(def game-state (atom [[0 1 0 0 0]
-                       [1 0 0 1 1]
-                       [1 1 0 0 1]
-                       [0 1 0 0 0]
-                       [1 0 0 0 1]]))
-
+(.text (js/$ "h1") "Game of Life")
 
 (defn render [board]
   (let [rows
@@ -22,6 +16,7 @@
         (.append "g")
         (.attr "transform"
                (fn [d i] (str "translate(0, " (* i 15) ")"))))
+
     (let [circles
           (-> rows
               (.selectAll "circle")
@@ -29,13 +24,25 @@
       (-> circles
           (.enter)
           (.append "circle"))
-      
+
       (-> circles
           (.attr "r" 5)
           (.attr "cx" (fn [d i] (+ 5 (* i 15))))
           (.attr "cy" 5)
           (.attr "class" (fn [d] ({0 "dead" 1 "live"} d)))))))
 
+(def game-state (atom nil))
 
-(render @game-state)
-(render (next-generation @game-state))
+(add-watch game-state :render
+           (fn [_ _ _ new-state]
+             (render new-state)))
+
+(reset! game-state [[0 1 0 0 0]
+                    [1 0 0 1 1]
+                    [1 1 0 0 1]
+                    [0 1 0 0 0]
+                    [1 0 0 0 1]])
+
+(go (while true
+      (<! (async/timeout 1000))
+      (swap! game-state next-generation)))
