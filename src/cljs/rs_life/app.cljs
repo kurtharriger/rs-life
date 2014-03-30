80be0165e1bab3893c4bbcff95c130ee4cfde90b
Kurt Harriger <kurtharriger@gmail.com>
2014-03-29 18:24:06 -0600

    D3 rendering of game state

--- src/cljs/rs_life/app.cljs
+++ src/cljs/rs_life/app.cljs
@@ -2,3 +2,40 @@
   (:require [rs-life.core :refer [next-generation]]))
 
 (.text (js/$ "h1") "It's alive!!")
+
+
+(def game-state (atom [[0 1 0 0 0]
+                       [1 0 0 1 1]
+                       [1 1 0 0 1]
+                       [0 1 0 0 0]
+                       [1 0 0 0 1]]))
+
+
+(defn render [board]
+  (let [rows
+        (->  js/d3
+             (.select ".game-of-life")
+             (.selectAll "g")
+             (.data (clj->js board)))]
+    (-> rows
+        (.enter)
+        (.append "g")
+        (.attr "transform"
+               (fn [d i] (str "translate(0, " (* i 15) ")"))))
+    (let [circles
+          (-> rows
+              (.selectAll "circle")
+              (.data identity))]
+      (-> circles
+          (.enter)
+          (.append "circle"))
+      
+      (-> circles
+          (.attr "r" 5)
+          (.attr "cx" (fn [d i] (+ 5 (* i 15))))
+          (.attr "cy" 5)
+          (.attr "class" (fn [d] ({0 "dead" 1 "live"} d)))))))
+
+
+(render @game-state)
+(render (next-generation @game-state))
